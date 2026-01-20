ebfbba9d2a62ab7be18ed714084f8f78b94f05a4
Ian <105128049+minijae011030@users.noreply.github.com>
2026-01-20 19:58:17 +0900

    fix: app 폴더 이름에 맞춰서 링크 수정

--- features/post/post-list/ui/post-item.tsx
+++ features/post/post-list/ui/post-item.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const PostItem = ({ post }: Props) => {
   return (
     <article key={post.postSeq} className="group border-b border-border pb-10">
-      <Link href={`/posts/${post.postSeq}`} className="block">
+      <Link href={`/post/${post.postSeq}`} className="block">
         <p className="mb-2 text-lg font-semibold text-muted-foreground">{post.category}</p>
         <h2 className="text-2xl font-semibold leading-tight tracking-tight decoration-point decoration-4 underline-offset-4 group-hover:underline">
           {post.title}
