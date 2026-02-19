e06de0d36ab3d21277ed2d7fbc5c7b48dc7987b2
Ian <105128049+minijae011030@users.noreply.github.com>
2026-02-19 17:19:10 +0900

    feat(api): 캐시 무효화 api 등록

---
+++ apps/api/src/common/common.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+import { RevalidateService } from './revalidate.service';
+
+@Module({
+  providers: [RevalidateService],
+  exports: [RevalidateService],
+})
+export class CommonModule {}
--- apps/api/src/post/post.module.ts
+++ apps/api/src/post/post.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
+import { CommonModule } from 'src/common/common.module';
 
 import { PostController } from './post.controller';
 import { PostMapper } from './post.mapper';
 import { PostService } from './post.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CommonModule],
   controllers: [PostController],
   providers: [PostService, PostMapper],
 })
